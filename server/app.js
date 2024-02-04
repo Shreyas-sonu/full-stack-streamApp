04a99269b2486f5a0ca797a6b405658bbd585bd8
Shreyas <sonureigns007@gmail.com>
2024-02-04 18:42:42 +0530

    server pages and sample videos added

---
+++ server/app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+const app = express();
+// add after 'const app = express();'
+
+app.get("/video", (req, res) => {
+  res.sendFile("assets/sample.mp4", { root: __dirname });
+});
+// add to end of file
+// add after app.get('/video/:id/data', ...) route
+
+app.get("/video/:id", (req, res) => {
+  const path = `assets/${req.params.id}.mp4`;
+  const stat = fs.statSync(path);
+  const fileSize = stat.size;
+  const range = req.headers.range;
+  if (range) {
+    const parts = range.replace(/bytes=/, "").split("-");
+    const start = parseInt(parts[0], 10);
+    const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+    const chunksize = end - start + 1;
+    const file = fs.createReadStream(path, { start, end });
+    const head = {
+      "Content-Range": `bytes ${start}-${end}/${fileSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": chunksize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(206, head);
+    file.pipe(res);
+  } else {
+    const head = {
+      "Content-Length": fileSize,
+      "Content-Type": "video/mp4",
+    };
+    res.writeHead(200, head);
+    fs.createReadStream(path).pipe(res);
+  }
+});
+app.listen(4000, () => {
+  console.log("Listening on port 4000!");
+});
